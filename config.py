d87838f3a6399ecc8044122d858899b027945183
HMD7SLK <sajd.haka11@gmail.com>
2026-06-25 06:09:08 +0200

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,13 @@ STRING_SESSION = os.getenv("STRING_SESSION", "")
 LOGGER_ID = int(os.getenv("LOGGER_ID", 0))
 
 GIT_TOKEN = os.getenv("GIT_TOKEN", "")
-UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "")
-UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "master")
+
+UPSTREAM_REPO = os.getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/HMD7SLK/My_Sourcehmd"
+)
+
+UPSTREAM_BRANCH = os.getenv(
+    "UPSTREAM_BRANCH",
+    "master"
+)
