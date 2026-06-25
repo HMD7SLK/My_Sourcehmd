b4bed5db8473497f66516249bafc29afe1c4f07b
HMD7SLK <sajd.haka11@gmail.com>
2026-06-25 06:18:33 +0200

    Refactor config.py to streamline environment variables
    Removed unused environment variables and added STRING2.

--- config.py
+++ config.py
@@ -1,14 +1,7 @@
-import os
-
-API_ID = int(os.getenv("API_ID", 0))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-
-MONGO_DB_URI = os.getenv("MONGO_DB_URI", "")
-
 OWNER_ID = int(os.getenv("OWNER_ID", 0))
 
 STRING1 = os.getenv("STRING1", "")
+STRING2 = os.getenv("STRING2", "")
 STRING_SESSION = os.getenv("STRING_SESSION", "")
 
 LOGGER_ID = int(os.getenv("LOGGER_ID", 0))
