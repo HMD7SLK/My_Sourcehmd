988e238c2b97f532d8411e9e031c4d28b82c999d
HMD7SLK <sajd.haka11@gmail.com>
2026-06-25 07:19:37 +0200

    Update misc.py

--- YousefMusic/misc.py
+++ YousefMusic/misc.py
@@ -5,10 +5,6 @@
 import socket
 import time
 
-def heroku():
-    global HAPP
-    HAPP = None
-    LOGGER(__name__).info("Heroku Disabled")
 from pyrogram import filters
 
 import config
@@ -23,7 +19,7 @@ _boot_ = time.time()
 
 
 def is_heroku():
-    return "heroku" in socket.getfqdn()
+    return False
 
 
 XCB = [
@@ -46,7 +42,8 @@ XCB = [
 def dbb():
     global db
     db = {}
-    LOGGER("ميــوزك اكس").info(f"تم تحديث قاعدة بيانات البوت ...✓")
+    LOGGER("ميــوزك اكس").info("تم تحديث قاعدة بيانات البوت ...✓")
+
 
 async def sudo():
     global SUDOERS
@@ -54,6 +51,7 @@ async def sudo():
     sudoersdb = mongodb.sudoers
     sudoers = await sudoersdb.find_one({"sudo": "sudo"})
     sudoers = [] if not sudoers else sudoers["sudoers"]
+
     if config.OWNER_ID not in sudoers:
         sudoers.append(config.OWNER_ID)
         await sudoersdb.update_one(
@@ -61,21 +59,14 @@ async def sudo():
             {"$set": {"sudoers": sudoers}},
             upsert=True,
         )
-    if sudoers:
-        for user_id in sudoers:
-            SUDOERS.add(user_id)
-    LOGGER(__name__).info(f"Sudoers Loaded.")
+
+    for user_id in sudoers:
+        SUDOERS.add(user_id)
+
+    LOGGER(__name__).info("Sudoers Loaded.")
 
 
 def heroku():
     global HAPP
-    if is_heroku:
-        if config.HEROKU_API_KEY and config.HEROKU_APP_NAME:
-            try:
-                Heroku = heroku3.from_key(config.HEROKU_API_KEY)
-                HAPP = Heroku.app(config.HEROKU_APP_NAME)
-                LOGGER(__name__).info(f"Heroku App Configured")
-            except BaseException:
-                LOGGER(__name__).warning(
-                    f"Please make sure your Heroku API Key and Your App name are configured correctly in the heroku."
-                )
+    HAPP = None
+    LOGGER(__name__).info("Heroku Disabled")
