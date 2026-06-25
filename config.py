d5d2a7b89a0101a2a2f4dbb7b692dd1524893029
HMD7SLK <sajd.haka11@gmail.com>
2026-06-25 06:07:13 +0200

    Fix environment variable defaults in config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "0"))
+API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
 MONGO_DB_URI = os.getenv("MONGO_DB_URI", "")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
-
+OWNER_ID = int(os.getenv("OWNER_ID", 0))
 STRING_SESSION = os.getenv("STRING_SESSION", "")
-LOGGER_ID = int(os.getenv("LOGGER_ID", "0"))
+LOGGER_ID = int(os.getenv("LOGGER_ID", 0))
 
+GIT_TOKEN = os.getenv("GIT_TOKEN", "")
 UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "")
 UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "master")
